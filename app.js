9e89f72f34e46ab025e9800b88420bfb68d6f0ac
Weirubo7 <447850103@qq.com>
2018-06-06 17:25:01 +0800

    third commit

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-// const express = require('express')
-// const app = express()
-
-// app.get('/', function (req, res) {
-//   res.send('Hello World!!!!')
-// })
-
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!')
-// })
 var express = require('express');
 var app = express();
  
@@ -51,3 +41,32 @@ var server = app.listen(8081, function () {
   // console.log("应用实例，访问地址为 http://%s:%s", host, port)
  
 })
+
+// var express = require('express');
+// var app = express();
+ 
+// app.use(express.static('public'));
+ 
+// app.get('/index.htm', function (req, res) {
+//    res.sendFile( __dirname + "/" + "index.htm" );
+// })
+ 
+// app.get('/process_get', function (req, res) {
+ 
+//    // 输出 JSON 格式
+//    var response = {
+//        "first_name":req.query.first_name,
+//        "last_name":req.query.last_name
+//    };
+//    console.log(response);
+//    res.end(JSON.stringify(response));
+// })
+ 
+// var server = app.listen(8081, function () {
+ 
+//   var host = server.address().address
+//   var port = server.address().port
+ 
+//   console.log("应用实例，访问地址为 http://%s:%s", host, port)
+ 
+// })
